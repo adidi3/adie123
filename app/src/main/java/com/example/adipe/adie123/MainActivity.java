0501a09888a3d374af79dcacb1b53276717f925e
adidi3 <ap0413@bs.amalnet.k12.il>
2018-04-23 15:05:02 +0300

    2 commit

--- app/src/main/java/com/example/adipe/adie123/MainActivity.java
+++ app/src/main/java/com/example/adipe/adie123/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.adipe.adie123;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        int x,n,j;
+
+
+    }
+
+
+
+    public void hilukk(View view) {
+    }
+
+    public void cefell(View view) {
     }
 }
